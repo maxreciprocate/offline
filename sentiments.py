2ad684b764b13168fce5619d26425c6bc4769cbe
reciprocated <56548574+reciprocated@users.noreply.github.com>
2022-09-26 23:13:05 +0300

    chore: simplify data loading

--- sentiments.py
+++ sentiments.py
@@ -14,58 +14,75 @@ import math
 
 from utils import batch_map, tohuman
 
-def load_tensors(cache_path, tokenizer, sentiment_pipe, max_length=128, use_cache=True):
+def load_tensors(name, get_text, get_reward, tokenizer, max_length=64, use_cache=True):
+    cache_path = f'cache/{name}_{max_length=}_tokenizer={tokenizer.name_or_path.split("/")[-1]}.pt'
     if use_cache and os.path.exists(cache_path):
-        cache = th.load(cache_path)
+        tensors = th.load(cache_path)
     else:
-        ds = load_dataset('imdb', split='train+test')
+        tensors = tokenizer(
+            get_text(),
+            max_length=max_length,
+            padding=True,
+            truncation='longest_first',
+            return_overflowing_tokens=True,
+            return_tensors='pt'
+        )
 
-        tokens = []
-        for text in tqdm(ds['text']):
-            ids = tokenizer(text)['input_ids']
-            ids.insert(0, tokenizer.eos_token_id)
-            ids.append(tokenizer.eos_token_id)
+        reward = get_reward(tensors['input_ids'])
+        reward = (reward - reward.mean()) / (reward.std() + 1e-30)
+        rewards = reward.view(-1, 1).repeat(1, max_length)
 
-            for i in range(0, max(1, len(ids)-max_length+1), max_length):
-                sids = ids[i:i + max_length]
-                tokens.append(sids + [tokenizer.eos_token_id] * (max_length - len(ids)))
-
-        tokens = tensor(tokens)
-        attention_masks = tokens.ne(tokenizer.eos_token_id).int()
-        attention_masks[:, 0] = 1
-
-        sentiments = batch_map(lambda batch: sentiment_pipe(tokenizer.batch_decode(batch)), tokens, bsize=1024, desc='Sentiments')
-        sentiments = tensor([-s['score'] if s['label'] == 'NEGATIVE' else s['score'] for s in sentiments])
-        rewards = sentiments.view(-1, 1).repeat(1, max_length-1)
-
-        cache = {'tokens': tokens, 'attention_masks': attention_masks, 'rewards': rewards}
+        rewards[tensors['attention_mask'].eq(tokenizer.pad_token_id)] = 0
+        tensors['rewards'] = rewards[:, :-1].contiguous()
 
         if not os.path.exists(os.path.dirname(cache_path)):
             os.mkdir(os.path.dirname(cache_path))
 
-        th.save(cache, cache_path)
+        th.save(tensors, cache_path)
 
-    print(f"{tohuman(np.prod(cache['tokens'].shape))} tokens")
-    return cache
+    print(f"{tohuman(np.prod(tensors['input_ids'].shape))} tokens")
+    return tensors
+
+def get_reward(sentiment_pipe, tokenizer, input_ids):
+    sentiments = batch_map(
+        lambda batch: sentiment_pipe(tokenizer.batch_decode(batch)),
+        input_ids, bsize=1024, desc='Sentiments'
+    )
+
+    return tensor([-s['score'] if s['label'] == 'NEGATIVE' else s['score'] for s in sentiments])
+
+def get_text(tokenizer):
+    ds = load_dataset('imdb', split='train+test')
+
+    def wrap_text(x):
+        x['text'] = tokenizer.bos_token + x['text'] + tokenizer.eos_token
+        return x
+
+    return ds.map(wrap_text)['text']
 
 class Sentiments:
-    def __init__(self, tokenizer: AutoTokenizer, max_length=64, n_samples=32, needs_pipe=False):
+    def __init__(self, tokenizer: AutoTokenizer, max_length=50, n_samples=256, needs_pipe=False):
         self.max_length = max_length
         self.tokenizer = tokenizer
-
-        cache_path = f'cache/imdb-sentiments_{max_length=}_tokenizer={tokenizer.name_or_path}.pt'
 
         if needs_pipe:
             self.sentiment_pipe = pipeline('sentiment-analysis', 'lvwerra/distilbert-imdb', device=th.device(0))
         else:
             self.sentiment_pipe = None
 
-        tensors = load_tensors(cache_path, self.tokenizer, self.sentiment_pipe, max_length=max_length)
+        tensors = load_tensors(
+            'sentiments',
+            get_text=partial(get_text, self.tokenizer),
+            get_reward=partial(get_reward, self.sentiment_pipe, self.tokenizer),
+            tokenizer=self.tokenizer,
+            max_length=max_length,
+            use_cache=True
+        )
 
         query = tensor([self.tokenizer.eos_token_id] * n_samples).view(n_samples, 1)
         self.logit_mask = None
 
-        self.dataset = TensorDataset(tensors['tokens'], tensors['attention_masks'], tensors['rewards'])
+        self.dataset = TensorDataset(tensors['input_ids'], tensors['attention_mask'], tensors['rewards'])
         self.eval_dataset = TensorDataset(query)
 
     def eval(self, samples, beta):
